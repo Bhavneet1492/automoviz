671b9add17c88c8f88ed0c924a166fd517ab19ab
blanc_sapphire <76656963+Bhavneet1492@users.noreply.github.com>
2022-06-04 21:00:56 +0530

    added comments

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+# importing streamlit and apps for each page from the "apps" folder
+
 import streamlit as st
 from apps import phase1,phase2,phase3,phase4,phase5
 
 
+# linking page names on sidebar to app files in the "app" folder
 
 PAGES = {
     "Data Cleaning": phase1,
@@ -9,9 +12,11 @@ PAGES = {
     "Make Predictions": phase3,
     "Business Insights": phase4,
     "Customer Segmentation": phase5,
-}
+} 
 
-#setting the page configuration
+
+# setting the page configuration
+
 st.set_page_config(
     page_title="Data Analysis",
     page_icon="📊",
@@ -19,7 +24,10 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
+
+# setting the sidebar for navigating through multiple apps (or pages or dashboards)
+
 st.sidebar.title('Navigation')
-selection = st.sidebar.selectbox("Select operation", list(PAGES.keys()))
-page = PAGES[selection]
-page.app()
+selection = st.sidebar.selectbox("Select operation", list(PAGES.keys()))  # option that the user chooses from the sidebar
+page = PAGES[selection] # PAGES value (app name from the "apps" folder) with user's choice as the key
+page.app() # running the app for the selected page choice 
